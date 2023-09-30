f583d92498b8168af36bda28f1bdd140a35c362f
Lucas Ferreira <94728279+yLexter@users.noreply.github.com>
2023-09-30 17:43:10 -0300

    Update atividade-2.py

--- Estatistica I/atividade-2.py
+++ Estatistica I/atividade-2.py
@@ -27,12 +27,14 @@ tamanhoEspacoAmostral = len(espacoAmostral)
 somaIgual7 = list(filter(lambda x: x[0] + x[1] == 7, espacoAmostral))
 numerosImpares = list(filter(lambda x: numeroImpar(x[0]) and numeroImpar(x[1]), espacoAmostral))
 produtoIgual12 =  list(filter(lambda x: x[0] * x[1] == 12, espacoAmostral))
+
 INTERSECAO_A_B = obterIntersecao(somaIgual7, numerosImpares)
 INTERSECAO_A_C = obterIntersecao(somaIgual7, produtoIgual12)
 
 P_A = len(somaIgual7) / tamanhoEspacoAmostral
 P_B = len(numerosImpares) / tamanhoEspacoAmostral
 P_C = len(produtoIgual12) / tamanhoEspacoAmostral
+
 P_A_INTERSECAO_B = len(INTERSECAO_A_B) / tamanhoEspacoAmostral
 P_A_INTERSECAO_C = len(INTERSECAO_A_C) / tamanhoEspacoAmostral
 
