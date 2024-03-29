d8d8431a7776080b675a756d982b71ba1d4b8cec
yLexter <lucasmaia5954@gmail.com>
2024-03-29 00:19:17 -0300

    remoção de pasta duplicada

--- Projetos/Quitanda/main.py
+++
@@ -1,7 +0,0 @@
-from functions import *
-
-db = DatabaseEstoque('dados')
-dbLogs = DatabaseLogs('logs')
-cliente = Client(db)
-
-menuInicial(db, dbLogs, cliente)
--- Projetos/README.md
+++
@@ -1,3 +0,0 @@
-# Projetos
-
-- Todos meus projetos feito ao longo do curso de ciências da computação na UEPB
