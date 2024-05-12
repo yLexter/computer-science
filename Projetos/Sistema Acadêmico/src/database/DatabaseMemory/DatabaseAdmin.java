63c8635d59d5784e67404ca6209c22f408e11f0f
yLexter <lucasmaia5954@gmail.com>
2024-05-12 00:08:39 -0300

    chore: remoção de pastas duplicadas

--- Projetos/Sistema Acadêmico/src/database/DatabaseMemory/DatabaseAdmin.java
+++
@@ -1,9 +0,0 @@
-package database.DatabaseMemory;
-
-import general.Admin;
-import interfaces.database.IDatabaseAdmin;
-
-/**
-  Classe para manusear os admins do banco de dados
-*/
-public class DatabaseAdmin extends DatabaseEmployee<Admin> implements IDatabaseAdmin {}
